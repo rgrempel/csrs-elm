683aa62ecb40453e08bf7de795cc78f85c678a10
Ryan Rempel <rgrempel@gmail.com>
2015-03-20 15:07:05 -0500

    Some actual editing of annuals. Woot!

--- src/main/webapp/scripts/app/tasks/processForms/contactAnnuals.js
+++ src/main/webapp/scripts/app/tasks/processForms/contactAnnuals.js
@@ -33,6 +33,7 @@ angular.module('csrsApp').directive("csrsContactAnnuals", function () {
 
         Annual.save({}, newAnnual, function (value, headers) {
             $http.get(headers('location')).success(function (data) {
+                data.editMode = true;
                 self.contact.annuals.push(data);
             }).error(function () {
                 // Shouldn't happen ...
@@ -49,5 +50,39 @@ angular.module('csrsApp').directive("csrsContactAnnuals", function () {
         }, function () {
             // Error
         })
+    };
+
+    this.edit = function (annual) {
+        annual.editMode = true;
+    };
+
+    this.commit = function (annual) {
+        // Need to massage this ... should figure out how to
+        // deal with this generally. Problem is that "contact"
+        // will be an integer, not an object, and the deserializaer
+        // at the other end expects the object. Could either deal
+        // with it at this end in the service, or at that end in
+        // the deserializer.
+        annual.contact = {
+            id: this.contact.id
+        };
+
+        Annual.update({}, annual, function () {
+            annual.editMode = false;
+        }, function () {
+            // error
+        });
+    };
+
+    this.cancel = function (annual) {
+        var self = contact;
+        Annual.get({
+            id: annual.id
+        }, function (result) {
+            _.pull(self.contact.annuals, annual);
+            self.contact.annuals.push(result);
+        }, function (error) {
+
+        });
     };
 });
