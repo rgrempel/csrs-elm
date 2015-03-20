f4ed7de1f62b2b8e122c63c99b4b28db090a97f1
Ryan Rempel <rgrempel@gmail.com>
2015-03-20 16:45:48 -0500

    By default, don't send the annuals with the contact.

--- src/main/webapp/scripts/components/entities/contact/contact.service.js
+++ src/main/webapp/scripts/components/entities/contact/contact.service.js
@@ -1,16 +1,34 @@
 'use strict';
 
-angular.module('csrsApp')
-    .factory('Contact', function ($resource) {
-        return $resource('api/contacts/:id', {}, {
-            'query': { method: 'GET', isArray: true},
-            'get': {
-                method: 'GET',
-                transformResponse: function (data) {
-                    data = angular.fromJson(data);
-                    return data;
-                }
-            },
-            'update': { method:'PUT' }
-        });
+angular.module('csrsApp').factory('Contact', function ($resource, _) {
+    return $resource('api/contacts/:id', {}, {
+        'query': {
+            method: 'GET',
+            isArray: true
+        },
+
+        'get': {
+            method: 'GET',
+            transformResponse: function (data) {
+                data = angular.fromJson(data);
+                return data;
+            }
+        },
+
+        'update': {
+            method: 'PUT',
+            transformRequest: function (data, headers) {
+                // By default, don't send the annuals ...
+                return  angular.toJson(_.omit(data, 'annuals'));
+            }
+        },
+    
+        'save': {
+            method: 'POST',
+            transformRequest: function (data, headers) {
+                // By default, don't send the annuals ...
+                return  angular.toJson(_.omit(data, 'annuals'));
+            }
+        }
     });
+});
