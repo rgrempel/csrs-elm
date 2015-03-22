6e5f1fe043ab209cdf9c549f598c4cccb62be04b
Ryan Rempel <rgrempel@gmail.com>
2015-03-22 00:39:39 -0500

    Start of a Latin translation.

--- src/main/webapp/scripts/components/language/language.service.js
+++ src/main/webapp/scripts/components/language/language.service.js
@@ -27,7 +27,7 @@ angular.module('csrsApp')
  They are written in English to avoid character encoding issues (not a perfect solution)
  */
     .constant('LANGUAGES', [
-        'en', 'fr'
+        'en', 'fr', 'la'
         //JHipster will add new languages here
     ]
 );
