ea2bf7624262784ce60ef09295f644782710a50e
Ryan Rempel <rgrempel@gmail.com>
2015-04-12 12:44:36 -0500

    More objectifying.

--- src/main/webapp/scripts/app/account/settings/membershipPricesFor.js
+++ src/main/webapp/scripts/app/account/settings/membershipPricesFor.js
@@ -6,7 +6,19 @@
     function membershipPricesForProvider ($q, _) {
         'ngInject';
 
-        var prices = {
+        return function membershipPricesFor (year) {
+            return $q.when(new MembershipPrices(_));
+        };
+    }
+
+    angular.extend(MembershipPrices.prototype, {
+        getPrice: getPrice
+    });
+
+    function MembershipPrices (_) {
+        this._ = _;
+
+        angular.extend(this, {
             membership: [{
                 code: 1,
                 rrCode: 1,
@@ -53,46 +65,42 @@
             },{
                 code: 2,
                 price: 1800
-            }],
+            }]
+        });
+    }
     
-            getPrice: function getPrice (renewal) {
-                /* jshint validthis: true */
-                var price = {};
+    function getPrice (renewal) {
+        /* jshint validthis: true */
+        var price = {};
 
-                var membership = _.find(this.membership, {
-                    code: renewal.membership
-                });
+        var membership = this._.find(this.membership, {
+            code: renewal.membership
+        });
 
-                price.a = membership.price;
+        price.a = membership.price;
 
-                if (renewal.rr > 0) {
-                    price.b = _.find(this.rr, {
-                        code: membership.rrCode
-                    }).price;
-                } else {
-                    price.b = 0;
-                }
+        if (renewal.rr > 0) {
+            price.b = this._.find(this.rr, {
+                code: membership.rrCode
+            }).price;
+        } else {
+            price.b = 0;
+        }
 
-                if (renewal.iter) {
-                    price.c = _.find(this.iter, {
-                        code: membership.iterCode
-                    }).price;
-                } else {
-                    price.c = 0;
-                }
+        if (renewal.iter) {
+            price.c = this._.find(this.iter, {
+                code: membership.iterCode
+            }).price;
+        } else {
+            price.c = 0;
+        }
 
-                price.d = price.a + price.b + price.c;
+        price.d = price.a + price.b + price.c;
 
-                price.e = (price.d * 3) - membership.reduction;
+        price.e = (price.d * 3) - membership.reduction;
 
-                price.f = renewal.duration == 1 ? price.d : price.e;
+        price.f = renewal.duration == 1 ? price.d : price.e;
 
-                return price;
-            }
-        };
-
-        return function membershipPricesFor (year) {
-            return $q.when(prices);
-        };
-    };
+        return price;
+    }
 })();
