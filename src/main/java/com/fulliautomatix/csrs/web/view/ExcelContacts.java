7f334a86fce6df37ffb3d6f440dd35f9c3452760
Ryan Rempel <rgrempel@gmail.com>
2015-10-19 18:24:31 -0500

    Additional work on Excel export -- still needs formatting.

--- src/main/java/com/fulliautomatix/csrs/web/view/ExcelContacts.java
+++ src/main/java/com/fulliautomatix/csrs/web/view/ExcelContacts.java
@@ -2,7 +2,10 @@ package com.fulliautomatix.csrs.web.view;
 
 import java.util.Date;
 import java.util.Map;
+import java.util.List;
+import java.util.Optional;
 import java.util.Collection;
+import java.util.Collections;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,6 +18,7 @@ import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFDataFormat;
 
 import com.fulliautomatix.csrs.domain.Contact;
+import com.fulliautomatix.csrs.domain.Annual;
 
 import org.springframework.web.servlet.view.document.AbstractExcelView;
 
@@ -25,7 +29,16 @@ public class ExcelContacts extends AbstractExcelView {
         HttpServletRequest request,
         HttpServletResponse response
     ) {
-        Collection<Contact> contacts = (Collection<Contact>) model.get("contacts");
+        Collection<Contact> contacts = 
+            (Collection<Contact>) model.get("contacts");
+        
+        List<Integer> years =
+            (List<Integer>) model.get("years");
+
+        Collections.sort
+            ( years
+            , Collections.reverseOrder()
+            );
 
         HSSFSheet sheet = workbook.createSheet("Spring");
         sheet.setDefaultColumnWidth(12);
@@ -41,11 +54,65 @@ public class ExcelContacts extends AbstractExcelView {
         cell.setCellValue(new Date());
         cell.setCellStyle(dateStyle);
 
+        // TODO: This should all be localized ...
+
+        // Write headers ...
+        HSSFRow headers = sheet.createRow(3);
+        headers.createCell(0).setCellValue("Full Name");
+        headers.createCell(1).setCellValue("Full Address");
+        headers.createCell(2).setCellValue("First Name");
+        headers.createCell(3).setCellValue("Last Name");
+        headers.createCell(4).setCellValue("City");
+        headers.createCell(5).setCellValue("Province");
+        headers.createCell(6).setCellValue("Country");
+        headers.createCell(7).setCellValue("Postal Code");
+        headers.createCell(8).setCellValue("Omit name");
+        headers.createCell(9).setCellValue("Omit email");
+        headers.createCell(10).setCellValue("Email");
+        headers.createCell(11).setCellValue("Interests");
+
+        int headerCell = 12;
+        HSSFRow headers2 = sheet.createRow(4);
+        
+        for (Integer year : years) {
+            headers.createCell(headerCell + 1).setCellValue(year);
+            
+            headers2.createCell(headerCell++).setCellValue("Membership");
+            headers2.createCell(headerCell++).setCellValue("Iter");
+            headers2.createCell(headerCell++).setCellValue("R & R");
+        }
+
         // Write contact names
-        int rowIndex = 4;
+        int rowIndex = 5;
         for (Contact c : contacts) {
             HSSFRow sheetRow = sheet.createRow(rowIndex++);
-            sheetRow.createCell(1).setCellValue(c.fullName());
+            sheetRow.createCell(0).setCellValue(c.fullName());
+            sheetRow.createCell(1).setCellValue(c.abbreviatedAddress());
+            sheetRow.createCell(2).setCellValue(c.getFirstName());
+            sheetRow.createCell(3).setCellValue(c.getLastName());
+            sheetRow.createCell(4).setCellValue(c.getCity());
+            sheetRow.createCell(5).setCellValue(c.getRegion());
+            sheetRow.createCell(6).setCellValue(c.getCountry());
+            sheetRow.createCell(7).setCellValue(c.getPostalCode());
+            sheetRow.createCell(8).setCellValue(c.getOmitNameFromDirectory());
+            sheetRow.createCell(9).setCellValue(c.getOmitEmailFromDirectory());
+            sheetRow.createCell(10).setCellValue(c.formattedEmail("\n"));
+            sheetRow.createCell(11).setCellValue(c.formattedInterests("\n"));
+
+            int rowCell = 12;
+            for (Integer year : years) {
+                Optional<Annual> annual = c.annualForYear(year);
+               
+                final int cellNumber = rowCell;
+
+                annual.ifPresent(a -> {
+                    sheetRow.createCell(cellNumber).setCellValue(a.getMembership());
+                    sheetRow.createCell(cellNumber + 1).setCellValue(a.getIter());
+                    sheetRow.createCell(cellNumber + 2).setCellValue(a.getRr());
+                });
+
+                rowCell += 3;
+            }
         }
 
         response.setHeader("Content-Disposition", "attachment; filename=\"csrs-contacts.xls\"");
