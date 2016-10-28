45908ee00ffcc1dc7d28f66c53e1e0362a59fafc
Ryan Rempel <rgrempel@gmail.com>
2016-10-28 14:49:15 -0500

    Allow sending invitations to specific people.

--- src/main/javascript/app/tasks/processForms/contactUsers.js
+++ src/main/javascript/app/tasks/processForms/contactUsers.js
@@ -12,11 +12,12 @@ angular.module('csrsApp').directive('csrsContactUsers', function () {
     };
 });
 
-angular.module('csrsApp').controller('ContactUsersCtrl', function (Contact, $scope) {
+angular.module('csrsApp').controller('ContactUsersCtrl', function (Contact, $scope, $http, $translate) {
     var self = this;
 
     this.users = [];
     this.fetched = false;
+    this.showSendInvitations = false;
 
     $scope.$watch(function () {
         return self.contact.id;
@@ -27,10 +28,26 @@ angular.module('csrsApp').controller('ContactUsersCtrl', function (Contact, $sco
             }, function (result) {
                 self.users = result;
                 self.fetched = true;
+                self.showSendInvitations = result.length == 0 && self.contact.contactEmails.length > 0
             }, function (error) {
                 alert(angular.toJson(error));
             });
         }
     });
+
+    this.sendInvitation = function () {
+        $http.post("/api/invitation/contacts", {
+            contactIDs: [self.contact.id]
+        }).success(function () {
+            $translate("membership.byYear.invitationsHaveBeenSent").then(function (msg) {
+                alert(msg);
+            });
+        }).error(function (data, status, headers, config) {
+            alert(angular.toJson(data));
+        });
+    };
 });
+
+
+
 
