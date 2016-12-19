e5a72920de142ecfe9ba938d640b43e95968d5c9
Ryan Rempel <rgrempel@gmail.com>
2016-12-19 00:22:47 -0600

    Re-enable fop configuration.

--- src/main/java/com/fulliautomatix/csrs/config/FopConfiguration.java
+++ src/main/java/com/fulliautomatix/csrs/config/FopConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Description;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.apache.fop.apps.FopFactory;
 import org.apache.fop.apps.FopFactoryBuilder;
+import org.apache.avalon.framework.configuration.DefaultConfigurationBuilder;
 import org.xml.sax.SAXException;
 import java.io.File;
 import javax.inject.Inject;
@@ -27,11 +28,23 @@ public class FopConfiguration {
     @Bean
     @Description("FopFactory")
     public FopFactory fopFactory () {
+        DefaultConfigurationBuilder cfgBuilder = new DefaultConfigurationBuilder();
+        
         FopFactoryBuilder builder = new FopFactoryBuilder(
-            new File("/home/csrs/config/fop.xml").toURI(),
+            new File(".").toURI(),
             graphicsResourceResolver
         );
         
+        try {
+            builder.setConfiguration(
+                cfgBuilder.buildFromFile(
+                    new File("/home/csrs/config/fop.xml")
+                )
+            );
+        } catch (Exception ex) {
+            log.error(ex.toString());
+        }
+   
         return builder.build();  
     }
 
